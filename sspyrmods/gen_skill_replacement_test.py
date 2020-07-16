92bac3a48a770eeb31e3f9db2614ae47ef6aa883
Angel LaVoie <tylerlavoie13@gmail.com>
2020-07-15 21:05:12 -0400

    Skill Randomizer

--- sspyrmods/gen_skill_replacement_test.py
+++ sspyrmods/gen_skill_replacement_test.py
@@ -1,5 +1,6 @@
 from bl3hotfixmod import Mod 
 
+
 mod=Mod('skill_replacement_test.txt',
 'Skill Replacement Test',
 'SSpyR',
@@ -15,30 +16,34 @@ mod.reg_hotfix(Mod.PATCH, '',
             'Tiers.Tiers[1].Object..Items.Items[0].Object..AbilityClass',
             Mod.get_full_cond('/Game/PlayerCharacters/Gunner/_Shared/_Design/Passives/_Tree_BottomlessMags/MatchedSet/Passive_Gunner_MatchedSet.Passive_Gunner_MatchedSet_C', 'BlueprintGeneratedClass'))
 
-mod.reg_hotfix(Mod.PATCH, '',
-            '/Game/PlayerCharacters/SirenBrawler/_Shared/_Design/Character/Inventory/AbilityTree_Siren_Brawl',
-            'Tiers.Tiers[1].Object..Items.Items[0].Object..ItemFrameName',
-            'matchingSet')
+#mod.reg_hotfix(Mod.PATCH, '',
+#            '/Game/PlayerCharacters/SirenBrawler/_Shared/_Design/Character/Inventory/AbilityTree_Siren_Brawl',
+#            'Tiers.Tiers[1].Object..Items.Items[0].Object..ItemFrameName',
+#            'matchingSet')
 
 mod.reg_hotfix(Mod.PATCH, '',
             '/Game/PlayerCharacters/SirenBrawler/_Shared/_Design/Character/Inventory/AbilityTree_Siren_Brawl',
             'Tiers.Tiers[1].Object..Items.Items[1].Object..AbilityClass',
             Mod.get_full_cond('/Game/PlayerCharacters/Gunner/_Shared/_Design/Passives/_Tree_BottomlessMags/MatchedSet/Passive_Gunner_MatchedSet.Passive_Gunner_MatchedSet_C', 'BlueprintGeneratedClass'))
 
-mod.reg_hotfix(Mod.PATCH, '',
-            '/Game/PlayerCharacters/SirenBrawler/_Shared/_Design/Character/Inventory/AbilityTree_Siren_Brawl',
-            'Tiers.Tiers[1].Object..Items.Items[1].Object..ItemFrameName',
-            'matchingSet')
+#mod.reg_hotfix(Mod.PATCH, '',
+#            '/Game/PlayerCharacters/SirenBrawler/_Shared/_Design/Character/Inventory/AbilityTree_Siren_Brawl',
+#            'Tiers.Tiers[1].Object..Items.Items[1].Object..ItemFrameName',
+#            'matchingSet')
 
 mod.reg_hotfix(Mod.PATCH, '',
             '/Game/PlayerCharacters/Gunner/_Shared/_Design/Character/Inventory/AbilityTree_Branch_BottomlessMag',
             'Tiers.Tiers[1].Object..Items.Items[1].Object..AbilityClass',
             Mod.get_full_cond('/Game/PlayerCharacters/SirenBrawler/_Shared/_Design/Passives/BrawlTree/RootToRise/PassiveSkill_Siren_RootToRise.PassiveSkill_Siren_RootToRise_C', 'BlueprintGeneratedClass'))
 
-mod.reg_hotfix(Mod.PATCH, '',
-            '/Game/PlayerCharacters/Gunner/_Shared/_Design/Character/Inventory/AbilityTree_Branch_BottomlessMag',
-            'Tiers.Tiers[1].Object..Items.Items[1].Object..ItemFrameName',
-            'rootsToRise')
+#mod.reg_hotfix(Mod.PATCH, '',
+#            '/Game/PlayerCharacters/Gunner/_Shared/_Design/Character/Inventory/AbilityTree_Branch_BottomlessMag',
+#            'Tiers.Tiers[1].Object..Items.Items[1].Object..ItemFrameName',
+#            'rootsToRise')
 
+mod.reg_hotfix(Mod.PATCH, '',
+            '/Game/PlayerCharacters/SirenBrawler/_Shared/_Design/Character/Inventory/AbilityTree_Siren_Brawl',
+            'Tiers.Tiers[1].Object..Items.Items[1].Object..ItemFrameName',
+            '(/Game/PlayerCharacters/Gunner/_Shared/_Design/Character/Inventory/AbilityTree_Branch_BottomlessMag.Tiers.Tiers[1].Object..Items.Items[1].Object..ItemFrameName)')
 
 mod.close()
